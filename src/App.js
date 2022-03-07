9cb54e18d127538ce841018acedd8203b7fdad86
SamuelDAlencar <sarl.2001@hotmail.com>
2022-03-07 17:01:40 -0300

    feat: header radio buttons to filter search addition

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <main>
-      <h1>Initial h1</h1>
+      <Header />
     </main>
   );
 }
