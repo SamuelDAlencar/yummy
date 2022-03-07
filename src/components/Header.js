b8fff14a56acce606643f38a0121a615946f5b30
Matheus Vaz <matheusvaz@matheuss-air.lan>
2022-03-07 14:23:08 -0800

    requirements 9 and 10

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import profileIcon from '../images/profileIcon.svg';
+import searchIcon from '../images/searchIcon.svg';
 
 export default function Header() {
   return (
     <header>
+      <section>
+        <button
+          type="submit"
+          data-testid="profile-top-btn"
+          id="profile-top-btn"
+        >
+          <img src={ profileIcon } data-testid="profile-top-btn" alt="profile-top-btn" />
+        </button>
+        <h2 data-testid="page-title">Foods</h2>
+        <button
+          type="submit"
+          data-testid="search-top-btn"
+          id="search-top-btn"
+        >
+          <img src={ searchIcon } data-testid="search-top-btn" alt="search-top-btn" />
+
+        </button>
+      </section>
       <label htmlFor="ingredients-search">
         Search by Ingredients
         <input
