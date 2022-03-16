4c22e04bb6b5d3b3654bdcd9f691f282178e257d
Alex Silva <alexcssilva@gmail.com>
2022-03-16 16:11:19 -0300

    style: page-profile-finish

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import Footer from '../components/Footer';
+import '../css/profile.css';
+import Header from '../components/Header';
 
 export default function Profile() {
   const getEmailLogin = JSON.parse(localStorage.getItem('user')).email;
@@ -12,31 +14,40 @@ export default function Profile() {
   };
 
   return (
-    <>
-      <h1 data-testid="page-title">Profile</h1>
+    <div>
+      <Header />
+      {/* <div className="profile">
+        <img
+          src={ profileIcon }
+          alt="profile-top-btn"
+        />
+        <h1 className="profile-h1" data-testid="page-title">Profile</h1>
+      </div> */}
       <h2 data-testid="profile-email">{ getEmailLogin }</h2>
-      <button
-        type="button"
-        data-testid="profile-done-btn"
-        onClick={ () => { history.push('/done-recipes'); } }
-      >
-        Done Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-favorite-btn"
-        onClick={ () => { history.push('/favorite-recipes'); } }
-      >
-        Favorite Recipes
-      </button>
-      <button
-        type="button"
-        data-testid="profile-logout-btn"
-        onClick={ handleLocalStorage }
-      >
-        Logout
-      </button>
+      <div className="container-button">
+        <button
+          type="button"
+          data-testid="profile-done-btn"
+          onClick={ () => { history.push('/done-recipes'); } }
+        >
+          Done Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-favorite-btn"
+          onClick={ () => { history.push('/favorite-recipes'); } }
+        >
+          Favorite Recipes
+        </button>
+        <button
+          type="button"
+          data-testid="profile-logout-btn"
+          onClick={ handleLocalStorage }
+        >
+          Logout
+        </button>
+      </div>
       <Footer />
-    </>
+    </div>
   );
 }
