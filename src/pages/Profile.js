20fc85cf2df8ef28266b28f833a12e9bcd9c4b5c
Alex Silva <alexcssilva@gmail.com>
2022-03-17 11:50:38 -0300

    style: page-profile-finish

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import profileIcon from '../images/profileIcon.svg';
 import Footer from '../components/Footer';
 import '../css/profile.css';
-import Header from '../components/Header';
 
 export default function Profile() {
   const getEmailLogin = JSON.parse(localStorage.getItem('user')).email;
@@ -14,18 +14,32 @@ export default function Profile() {
   };
 
   return (
-    <div>
-      <Header />
-      {/* <div className="profile">
+    <div className="container-profile">
+      <h1 className="profile-h1" data-testid="page-title">Profile</h1>
+      <div className="container-image-profile">
         <img
+          className="image-profile"
           src={ profileIcon }
           alt="profile-top-btn"
         />
-        <h1 className="profile-h1" data-testid="page-title">Profile</h1>
-      </div> */}
-      <h2 data-testid="profile-email">{ getEmailLogin }</h2>
+        <inpu
+          name="lar"
+          className="profile-picture-button"
+          type="button"
+        >
+          Profile Picture
+        </inpu>
+      </div>
+      <h2
+        className="profile-email"
+        data-testid="profile-email"
+      >
+        { getEmailLogin }
+
+      </h2>
       <div className="container-button">
         <button
+          className="profile-button"
           type="button"
           data-testid="profile-done-btn"
           onClick={ () => { history.push('/done-recipes'); } }
@@ -33,6 +47,7 @@ export default function Profile() {
           Done Recipes
         </button>
         <button
+          className="profile-button"
           type="button"
           data-testid="profile-favorite-btn"
           onClick={ () => { history.push('/favorite-recipes'); } }
@@ -40,6 +55,7 @@ export default function Profile() {
           Favorite Recipes
         </button>
         <button
+          className="button-logout"
           type="button"
           data-testid="profile-logout-btn"
           onClick={ handleLocalStorage }
