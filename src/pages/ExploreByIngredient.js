2c81fbfc5c8fa3394d8da59ee489b3348f6117e7
Matheus Vaz <matheusvaz@Matheuss-MacBook-Air.local>
2022-03-20 16:30:29 -0700

    add card image to explore by ingredients page

--- src/pages/ExploreByIngredient.js
+++ src/pages/ExploreByIngredient.js
@@ -41,6 +41,7 @@ export default function ExploreByIngredient() {
             name={ o[`strIngredient${complement}`] }
             index={ i }
             type={ apiType }
+            cardType="recipe"
           />
         </Link>
       )) }
